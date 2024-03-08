5c333cdb2ad3ffcecc33c8f9e4047ad46066e524
Pranata Jaya Negara <pranatajayanegara@gmail.com>
2024-03-09 04:47:00 +0700

    h

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import { postsRoutes } from "./routes/postsRoutes.js";
 import { usersRoutes } from "./routes/usersRoutes.js";
 import mongoose from "mongoose";
+import cors from "cors";
 
 import path from "path";
 import { fileURLToPath } from "url";
@@ -12,6 +13,7 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/posts", postsRoutes);

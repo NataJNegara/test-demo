290664b690efb43fd10f34c9be4fa75d3d5ea822
Pranata Jaya Negara <pranatajayanegara@gmail.com>
2024-03-10 13:42:44 +0700

    update

--- server/server.js
+++ server/server.js
@@ -8,23 +8,25 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 // // resolving dirname for ES Module
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
 app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/api/posts", postsRoutes);
 app.use("/api/users", usersRoutes);
 
-// // use the client app
-// app.use(express.static(path.join(__dirname, "/client/dist")));
-// // render client for any path
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "/client/dist/index.html"));
-// });
+// use the client app
+app.use(express.static(path.join(__dirname, "/client/dist")));
+// render client for any path
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/dist/index.html"));
+});
 
 mongoose
   .connect(process.env.DB_URI, { dbName: "mern-demo" })
